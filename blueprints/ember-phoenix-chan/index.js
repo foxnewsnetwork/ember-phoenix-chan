5a3c049b73e7055f9d0db95489646765d7b38699
Thomas Chen <foxnewsnetwork@gmail.com>
2015-11-19 10:39:44 -0800

    playing with addBower2

--- blueprints/ember-phoenix-chan/index.js
+++ blueprints/ember-phoenix-chan/index.js
@@ -1,13 +1,9 @@
-var RSVP = require('rsvp');
-
 module.exports = {
   description: 'grabs phoenix from github',
 
   normalizeEntityName: function() {},
 
   afterInstall: function() {
-    return RSVP.all([
-      this.addBowerPackagesToProject('phoenixframework/phoenix#v1.0.3')
-    ]);
+    return this.addBowerPackagesToProject('phoenixframework/phoenix#v1.0.3');
   }
 }
