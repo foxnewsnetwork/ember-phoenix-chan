f7793e35a216a392ff0d0f42679e01ed00e9d948
Thomas Chen <foxnewsnetwork@gmail.com>
2016-03-03 21:37:43 -0800

    updated to phoenix 1.1.4

--- blueprints/ember-phoenix-chan/index.js
+++ blueprints/ember-phoenix-chan/index.js
@@ -4,6 +4,6 @@ module.exports = {
   normalizeEntityName: function() {},
 
   afterInstall: function() {
-    return this.addBowerPackageToProject('phoenix', 'phoenixframework/phoenix#v1.0.3');
+    return this.addBowerPackageToProject('phoenix', 'phoenixframework/phoenix#v1.1.4');
   }
 }
